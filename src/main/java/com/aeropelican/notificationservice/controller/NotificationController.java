b2e9c4c7d4a60d7e8a1f21ca9e6eedb15df1d30c
Shareef Mastan <shareefmastan35@gmail.com>
2026-09-09 15:39:28 +0530

    Implemented notification Controller and entity by ID API

---
+++ src/main/java/com/aeropelican/notificationservice/controller/NotificationController.java
@@ -0,0 +1,27 @@
+package com.aeropelican.notificationservice.controller;
+
+import com.aeropelican.notificationservice.dto.NotificationResponse;
+import com.aeropelican.notificationservice.service.NotificationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NotificationResponse> getNotificationById(
+            @PathVariable Long id) {
+
+        NotificationResponse response =
+                notificationService.getNotificationById(id);
+
+        return ResponseEntity.ok(response);
+    }
+}
